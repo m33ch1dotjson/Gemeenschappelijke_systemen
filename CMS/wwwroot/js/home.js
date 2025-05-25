ec6c66018bf59b327e5ff482aade146043074fea
Mika <Mika.kleinbergen@gmail.com>
2025-05-25 21:06:51 +0200

    feat: Added a slider to the homepage.

---
+++ CMS/wwwroot/js/home.js
@@ -0,0 +1,11 @@
+﻿const sliderrail_element = document.getElementById("sliderrail")
+let amount_of_slides = document.querySelectorAll("#sliderrail div").length
+let current_slide = 1
+
+setInterval(() => {
+    current_slide++
+    if (current_slide > amount_of_slides) {current_slide = 1 }
+    sliderrail_element.style.left = "-" + ((current_slide-1)*600)+"px"
+},3000)
+
+console.log(current_slide)
